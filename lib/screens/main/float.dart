ec06f784501d57298a58e90596c63c46817de117
atifahzailani <nuratifahipot@gmail.com>
2021-07-04 16:04:40 +0800

    Create float.dart

---
+++ lib/screens/main/float.dart
@@ -0,0 +1,49 @@
+// TODO Complete the file, main/float.dart
+//-----------------------------------------------------------------------------------------------------------------------------
+//? Things to do:
+//   1. Perform the following operations:
+//        a. Add a new todo  - i.e. when the user taps on the 'plus' button
+//           This operation will navigate to the '/new' route.
+//        b. Refresh the screen - i.e. when the user taps on the 'Refresh' button
+//-----------------------------------------------------------------------------------------------------------------------------
+
+import 'package:flutter/material.dart';
+import '../../models/todo.dart';
+import 'main_screen.dart';
+
+class Float extends StatelessWidget {
+  const Float({state}) : _state = state;
+
+  final MainScreenState _state;
+
+  void _onAddPressed(BuildContext context) async {
+    var _todo = await Navigator.pushNamed(context, '/new');
+
+    if (_todo != null) {
+      _state.addTodo(_todo);
+    }
+  }
+
+  void onRefresh() => _state.refreshTodoListFuture();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          tooltip: 'Add a new todo',
+          child: Icon(Icons.add),
+          heroTag: null,
+          onPressed: () => _onAddPressed(context),
+        ),
+        FloatingActionButton(
+            tooltip: 'Refresh',
+            child: Icon(Icons.refresh),
+            heroTag: null,
+            onPressed: () => onRefresh())
+      ],
+    );
+  }
+}
+
